c0534f19e0387b0dbbc299c1e4fa033c4e7db0c0
Weslley Luiz Kampa <91283681+agr3w@users.noreply.github.com>
2025-04-24 20:45:50 -0300

    Delete src directory

--- src/data/specsData.js
+++
@@ -1,9 +0,0 @@
-// src/data/specsData.js
-const specsData = [
-  { label: "Dimensões", value: "150×50×40 cm" },
-  { label: "Peso", value: "12 kg" },
-  { label: "Alimentação", value: "110–220V" },
-  { label: "Garantia", value: "12 meses" },
-];
-
-export default specsData;
