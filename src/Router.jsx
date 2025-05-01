58c019d3b638458dbc8be5c3b27af610ea3f4dba
agr3w <suzanakampa.sk@gmail.com>
2025-05-01 19:15:56 -0300

    Implementando a pagina de checkout

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,8 +1,3 @@
-/**  
-  Arquivo de rotas base.
-  Aqui, você pode adicionar suas páginas conforme necessário.
-*/
-
 import { Route, Routes } from "react-router";
 import Login from "./pages/login/LoginRender";
 import Home from "./pages/home/Home";
@@ -11,6 +6,18 @@ import Cadastro from "./components/Cadastro/Cadastro";
 import TotensListRender from "./pages/totensList/TotensListRender";
 import useScrollToTop from "./hook/useScrollToTop";
 import CartPage from "./pages/Cart/CartPage";
+import CheckoutPage from "./pages/checkout/CheckoutPage";
+
+/**
+ * @file AppRouter.jsx
+ * @description
+ * Arquivo de rotas base.
+ * Aqui, você pode adicionar suas páginas conforme necessário.
+ * As rotas são definidas usando o componente <Routes> do React Router.
+ * Cada rota é definida usando o componente <Route>, onde você especifica o caminho (path) e o componente a ser renderizado (element).
+ * Você pode adicionar mais rotas conforme necessário, seguindo o mesmo padrão.
+ * Certifique-se de que os componentes importados estejam corretos e disponíveis no caminho especificado.
+ */
 
 const AppRouter = () => {
   useScrollToTop();
@@ -23,6 +30,7 @@ const AppRouter = () => {
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/totens" element={<TotensListRender />} />
         <Route path="/carrinho" element={<CartPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
       </Routes>
     </>
   );
