269ceaf8b400f740d6ca46940f06b36f7ca98dc9
agr3w <suzanakampa.sk@gmail.com>
2025-05-16 10:20:50 -0300

    LocalStorage para persistir o cliente logado

--- src/hook/useAuth.jsx
+++ src/hook/useAuth.jsx
@@ -1,13 +1,35 @@
-// src/context/AuthContext.js
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [user, setUser] = useState(null);
+  useEffect(() => {
+  const loginTime = localStorage.getItem("loginTime");
+  if (loginTime) {
+    const now = Date.now();
+    const maxTime = 1000 * 60 * 60 * 4; // 4 horas
+    if (now - loginTime > maxTime) {
+      logout();
+    }
+  }
+}, []);
+  // Tenta recuperar do localStorage
+  const [user, setUser] = useState(() => {
+    const saved = localStorage.getItem("user");
+    return saved ? JSON.parse(saved) : null;
+  });
 
-  const login = data => setUser(data);
-  const logout = () => setUser(null);
+  const login = data => {
+    setUser(data);
+    localStorage.setItem("user", JSON.stringify(data));
+    localStorage.setItem("loginTime", Date.now());
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+    localStorage.removeItem("loginTime");
+  };
 
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
